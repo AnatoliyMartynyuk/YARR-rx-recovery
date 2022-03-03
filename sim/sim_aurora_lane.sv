e99dc5aaf0917f8138e5223f3cb277b572791e22
AnatoliyMartynyuk <anatoli2@uw.edu>
2022-03-02 23:23:11 -0800

    Implemented reasonable monitoring
    Monitoring is based on the lanes output_valid signal. We check the moment the valid signal went low and track the time it took to go back on again. We also count the number of slips that occurred in that time frame.

--- sim/sim_aurora_lane.sv
+++ sim/sim_aurora_lane.sv
@@ -133,4 +133,54 @@ module sim_aurora_lane();
     end
     // ----------------------------------------------------------------------
 
+    // ----------------------------------------------------------------------
+    //              
+    // ----------------------------------------------------------------------
+
+    // ----------------------------------------------------------------------
+    //                          Monitoring
+
+    realtime time_1 = 0;
+    realtime time_2 = 0;
+
+    integer serdes_slip_cntr, gbox_slip_cntr;
+    logic serdes_slip_d1, gbox_slip_d1; 
+
+
+
+
+    // monitors disruptions in lane output data
+    initial begin
+        $timeformat(-9, 2, "ns");
+        forever begin
+            wait(rx_valid == 0);
+            time_1 = $realtime;
+            wait(rx_valid == 1);
+            //if ($realtime - time_1 > 45ns) $display("large rx_valid delay detected at %t. Duration: %t", $realtime, $realtime - time_1); 
+            if ($realtime - time_1 > 96ns) begin
+                $display("rx_valid went low at time: %t. Duration: %t", time_1, $realtime - time_1); 
+                $display("Saw %d serdes slips and %d gearbox slips", serdes_slip_cntr, gbox_slip_cntr);
+            end
+        end
+    end
+
+    // counts slips for gearbox and serdes
+    always_ff @(posedge clk_ddr_i) begin
+        if (~rst_n_i | rx_valid) begin
+            serdes_slip_cntr <= 0;
+            gbox_slip_cntr   <= 0;
+        end
+
+        else begin
+            serdes_slip_d1 <= u_aurora_rx_lane.serdes_slip;
+            gbox_slip_d1 <= u_aurora_rx_lane.gearbox_slip;
+
+            if (~serdes_slip_d1 & u_aurora_rx_lane.serdes_slip) serdes_slip_cntr <= serdes_slip_cntr + 1;
+            if (~gbox_slip_d1 & u_aurora_rx_lane.gearbox_slip) gbox_slip_cntr <= gbox_slip_cntr + 1;
+        end
+    end
+    
+    // ----------------------------------------------------------------------
+
+
 endmodule
